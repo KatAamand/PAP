a0059aa05292133f6eb04137b7e539a68d5eca99
KatAamand <143415617+KatAamand@users.noreply.github.com>
2025-03-01 11:09:02 +0100

    Updating server to run on dynamic port for Render

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express'); 
 const app = express(); 
-const port = 3000; 
+const port = process.env.Port || 3000; 
 
 app.set('view engine', 'ejs'); 
 app.use(express.urlencoded({ extended: true})); 
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+    console.log(`Server is running on port: ${port}`)
 });
 
 
