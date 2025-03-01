f0d1742d42ec065c2a24975e63c950ccbbb6a004
KatAamand <143415617+KatAamand@users.noreply.github.com>
2025-03-01 12:10:18 +0100

    Fixing bugs for deployment

--- src/routes/invoiceRoutes.js
+++ src/routes/invoiceRoutes.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const invoiceController = require('../controllers/invoiceController');
 
 router.get('/', invoiceController.getInvoicePage);
-router.post('/generate', invoiceController.generateInvoice);
-router.get('/preview', invoiceController.previewInvoice);
-router.get('/approve', invoiceController.approveInvoice)
+
 
 router.get('/products', (req, res) => {
     res.render('products', { products });
--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ app.use('/', routes);
 const products = require('./models/products');
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 });
